8038b403b660cad0d7fe5647b75ec562886c4b02
Michael <goldtextwitch@outlook.com>
2018-06-14 21:44:16 -0700

    Fix Switch BATs using wrong endian

--- Convert to BFSTM (Switch).bat
+++ Convert to BFSTM (Switch).bat
@@ -2,5 +2,5 @@
 cd /d "%~dp0"
 vgmstream -l 1 -f 0 -L -o "%~n1.lwav" "%~1"
 move "%~n1.lwav" "."
-vgaudio --big-endian "%~n1.lwav" "%~n1.bfstm"
+vgaudio --little-endian "%~n1.lwav" "%~n1.bfstm"
 del "%~n1.lwav"
