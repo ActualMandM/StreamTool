f8b6b27035760af48a27a44d16d37cd73e1e0c7b
Michael <goldtextwitch@outlook.com>
2018-11-30 20:53:00 -0800

    Add OPUS workaround to IDSP script For some reason converting OPUS directly to IDSP causes the file to be bad...

--- Mass Convert to IDSP.bat
+++ Mass Convert to IDSP.bat
@@ -1,5 +1,9 @@
 @echo off
 cd /d "%~dp0"
 for %%f in ("mcinput/*") do vgmstream -l 1 -f 0 -L -o "mcoutput\%%~nf.wav" "mcinput\%%f"
+for %%f in ("mcinput/*") do vgaudio "mcoutput\%%~nf.wav" "mcoutput\%%~nf.brstm"
+for %%f in ("mcinput/*") do del "mcoutput\%%~nf.wav"
+for %%f in ("mcinput/*") do vgmstream -l 1 -f 0 -L -o "mcoutput\%%~nf.wav" "mcoutput\%%~nf.brstm"
+for %%f in ("mcinput/*") do del "mcoutput\%%~nf.brstm"
 for %%f in ("mcinput/*") do vgaudio "mcoutput\%%~nf.wav" "mcoutput\%%~nf.idsp"
 for %%f in ("mcinput/*") do del "mcoutput\%%~nf.wav"
