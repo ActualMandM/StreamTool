ea8036f294f0ba8e77bb9338afe6b105f81bf3f9
Michael <goldtextwitch@outlook.com>
2019-03-17 23:23:43 -0700

    Create higher quality MP3s

--- Convert to MP3.bat
+++ Convert to MP3.bat
@@ -4,6 +4,6 @@ echo Please input the set of stereo channels that you want. Be aware that first 
 set /P S_C="Please input your number (Default is 0): "
 cd /d "%~dp0"
 vgmstream -2 %S_C% -o "%~n1.wav" "%~1"
-ffmpeg -i "%~n1.wav" "%~n1.mp3"
+ffmpeg -i "%~n1.wav" -q:a 0 "%~n1.mp3"
 del "%~n1.wav"
 move "%~n1.mp3" "."
