ddf870c8d50f3fe52bb30ca8c98f67d79a4dc3f0
Michael <goldtextwitch@outlook.com>
2019-03-29 21:57:22 -0700

    Show metadata in command window when outputting loop samples

--- Mass Convert to WAV (Output Loop Samples).bat
+++ Mass Convert to WAV (Output Loop Samples).bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d "%~dp0"
-for %%f in ("mcinput/*") do vgmstream -l 1 -f 0 -o "mcoutput\%%~nf.wav" "mcinput\%%f" > "mcoutput\%%~nf.txt"
+for %%f in ("mcinput/*") do vgmstream -l 1 -f 0 -o "mcoutput\%%~nf.wav" "mcinput\%%f"
+for %%f in ("mcinput/*") do vgmstream -m "mcinput\%%f" > "mcoutput\%%~nf.txt"
