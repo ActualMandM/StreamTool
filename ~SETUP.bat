da0176e6bf9369133d8548e474646ae8c63dda8c
Michael <goldtextwitch@outlook.com>
2019-07-10 10:59:33 -0700

    Add support for downloading ffmpeg

--- ~SETUP.bat
+++ ~SETUP.bat
@@ -5,6 +5,8 @@ echo Downloading vgaudio...
 powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('https://ci.appveyor.com/api/projects/Thealexbarney/VGAudio/artifacts/VGAudioCli.exe?branch=master', 'vgaudio.exe')"
 echo Downloading oggenc...
 powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('http://www.rarewares.org/files/ogg/oggenc2.88-1.3.5-generic.zip', 'oggenc.zip')"
+echo Updating ffmpeg...
+powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-latest-win32-shared.zip', 'ffmpeg.zip')"
 echo Renaming 7zip...
 ren 7za 7za.exe
 echo Extracting vgmstream...
@@ -13,22 +15,63 @@ ren test.exe vgmstream.exe
 echo Extracting oggenc...
 7za x "oggenc.zip" -y
 ren oggenc2.exe oggenc.exe
-echo Opening ffmpeg site...
-echo Download the nightly 32-bit shared version, and extract the contents of the bin folder into this directory.
-ffmpeg.url
-pause
+echo Extracting ffmpeg...
+7za e "ffmpeg.zip" -y
 echo Making mass convert folders...
 if not exist mcinput mkdir mcinput
 if not exist mcoutput mkdir mcoutput
 echo Deleting unnecessary files...
-del "appveyor.yml"
 del COPYING
 del README.md
-del ffmpeg.url
 del ffplay.exe
 del ffprobe.exe
 del in_vgmstream.dll
 del oggenc.zip
 del vgmstream.zip
 del xmp-vgmstream.dll
+rmdir bin
+rmdir doc
+rmdir ffmpeg-latest-win32-shared
+rmdir presets
+del LICENSE.txt
+del README.txt
+del bootstrap.min.css
+del default.css
+del developer.html
+del faq.html
+del fate.html
+del ffmpeg-all.html
+del ffmpeg-bitstream-filters.html
+del ffmpeg-codecs.html
+del ffmpeg-devices.html
+del ffmpeg-filters.html
+del ffmpeg-formats.html
+del ffmpeg-protocols.html
+del ffmpeg-resampler.html
+del ffmpeg-scaler.html
+del ffmpeg-utils.html
+del ffmpeg.html
+del ffplay-all.html
+del ffplay.html
+del ffprobe-all.html
+del ffprobe.html
+del general.html
+del git-howto.html
+del libavcodec.html
+del libavdevice.html
+del libavfilter.html
+del libavformat.html
+del libavutil.html
+del libswresample.html
+del libswscale.html
+del mailing-list-faq.html
+del nut.html
+del platform.html
+del style.min.css
+del ffprobe.xsd
+del libvpx-1080p.ffpreset
+del libvpx-1080p50_60.ffpreset
+del libvpx-360p.ffpreset
+del libvpx-720p.ffpreset
+del libvpx-720p50_60.ffpreset
 del "~SETUP.bat"
--- ~UPDATE.bat
+++ ~UPDATE.bat
@@ -12,17 +12,16 @@ del libmpg123-*.dll
 del libvorbis.dll
 del swresample-vgmstream-*.dll
 del vgmstream.exe
-REM echo Deleting ffmpeg...
-REM del avcodec-*.dll
-REM del avdevice-*.dll
-REM del avfilter-*.dll
-REM del avformat-*.dll
-REM del avutil-*.dll
-REM del ffmpeg.exe
-REM del ffplay.exe
-REM del postproc-*.dll
-REM del swresample-*.dll
-REM del swscale-*.dll
+echo Deleting ffmpeg...
+del avcodec-*.dll
+del avdevice-*.dll
+del avfilter-*.dll
+del avformat-*.dll
+del avutil-*.dll
+del ffmpeg.exe
+del postproc-*.dll
+del swresample-*.dll
+del swscale-*.dll
 echo Deleting vgaudio...
 del vgaudio.exe
 echo Updating vgmstream...
@@ -31,11 +30,62 @@ echo Updating vgaudio...
 powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('https://ci.appveyor.com/api/projects/Thealexbarney/VGAudio/artifacts/VGAudioCli.exe?branch=master', 'vgaudio.exe')"
 echo Extracting vgmstream...
 7za x "vgmstream.zip"
+echo Updating ffmpeg...
+powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-latest-win32-shared.zip', 'ffmpeg.zip')"
+echo Extracting ffmpeg...
+7za e "ffmpeg.zip" -y
 ren test.exe vgmstream.exe
 echo Deleting unnecessary files...
 del COPYING
 del README.md
-REM del ffprobe.exe
+del ffplay.exe
+del ffprobe.exe
 del in_vgmstream.dll
 del vgmstream.zip
+del ffmpeg.zip
 del xmp-vgmstream.dll
+rmdir bin
+rmdir doc
+rmdir ffmpeg-latest-win32-shared
+rmdir presets
+del LICENSE.txt
+del README.txt
+del bootstrap.min.css
+del default.css
+del developer.html
+del faq.html
+del fate.html
+del ffmpeg-all.html
+del ffmpeg-bitstream-filters.html
+del ffmpeg-codecs.html
+del ffmpeg-devices.html
+del ffmpeg-filters.html
+del ffmpeg-formats.html
+del ffmpeg-protocols.html
+del ffmpeg-resampler.html
+del ffmpeg-scaler.html
+del ffmpeg-utils.html
+del ffmpeg.html
+del ffplay-all.html
+del ffplay.html
+del ffprobe-all.html
+del ffprobe.html
+del general.html
+del git-howto.html
+del libavcodec.html
+del libavdevice.html
+del libavfilter.html
+del libavformat.html
+del libavutil.html
+del libswresample.html
+del libswscale.html
+del mailing-list-faq.html
+del nut.html
+del platform.html
+del style.min.css
+del ffprobe.xsd
+del libvpx-1080p.ffpreset
+del libvpx-1080p50_60.ffpreset
+del libvpx-360p.ffpreset
+del libvpx-720p.ffpreset
+del libvpx-720p50_60.ffpreset
