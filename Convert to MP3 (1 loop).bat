d3b0c112b4be18b6d81194fc7d76cdfa9c0a5a5f
Michele <goldtextwitch@outlook.com>
2019-12-21 17:29:45 -0800

    Increase OGG quality + add 1 loop
    Also specify codec for OGG and MP3

--- Convert to MP3 (1 loop).bat
+++ Convert to MP3 (1 loop).bat
@@ -3,6 +3,6 @@ echo This script will only convert the first set of stereo channels.
 echo Please downmix via TXTP if you want to hear more channels.
 cd /d "%~dp0"
 vgmstream -l 1 -2 0 -o "%~n1.wav" "%~1"
-ffmpeg -i "%~n1.wav" -b:a 320k "%~n1.mp3"
+ffmpeg -i "%~n1.wav" -codec:a libmp3lame -b:a 320k "%~n1.mp3"
 del "%~n1.wav"
 move "%~n1.mp3" "."
--- Convert to OGG (1 loop).bat
+++ Convert to OGG (1 loop).bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
-vgmstream -o "%~n1.wav" "%~1"
+vgmstream -l 1 -2 0 -o "%~n1.wav" "%~1"
 ffmpeg -i "%~n1.wav" -codec:a libvorbis -qscale:a 9 "%~n1.ogg"
 del "%~n1.wav"
 move "%~n1.ogg" "."
