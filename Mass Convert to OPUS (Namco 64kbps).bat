b1160c6748bd4e32f313b8a36d955cdfd25eaa63
M&M <goldtextwitch@outlook.com>
2020-08-23 23:27:07 -0700

    Add 192kbps option for Namco OPUS
    SSBU music mods with this conversion option require the Skyline plugin "replace_music"

--- Mass Convert to OPUS (Namco 64kbps).bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-for %%f in ("mcinput/*") do vgmstream -l 1 -f 0 -L -o "mcoutput\%%~nf.wav" "mcinput\%%f"
-for %%f in ("mcinput/*") do vgaudio --opusheader namco --bitrate 64000 --cbr "mcoutput\%%~nf.wav" "mcoutput\%%~nf.lopus"
-for %%f in ("mcinput/*") do del "mcoutput\%%~nf.wav"
