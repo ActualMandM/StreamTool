fb7df73bb1f83d8b926b2bd758c5ceec437d2dfe
Michael <15317421+ActualMandM@users.noreply.github.com>
2022-09-09 00:27:09 -0700

    Remove script for looping OGG.

--- Convert to Looping OGG.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-vgmstream -g -l 1 -f 0 -L -o "%~n1.wav" "%~1" > "%~n1.bat"
-call "%~n1.bat"
-move "%~n1.ogg" "."
-ren "%~n1.ogg" "%~n1.logg"
-del "%~n1.bat"
-del "%~n1.wav"
