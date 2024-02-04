6acd5ec584e06a930c2620baf9e79ecdbbd8eb3a
Michael <15317421+ActualMandM@users.noreply.github.com>
2024-02-03 18:06:26 -0800

    Use Highest quality for HCAs.

--- Convert to HCA.bat
+++ Convert to HCA.bat
@@ -2,5 +2,5 @@
 cd /d "%~dp0"
 vgmstream -l 1 -f 0 -L -o "%~n1.wav" "%~1"
 move "%~n1.wav" "."
-vgaudio --hcaquality High "%~n1.wav" "%~n1.hca"
+vgaudio --hcaquality Highest "%~n1.wav" "%~n1.hca"
 del "%~n1.wav"
